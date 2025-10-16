81cfc7396fcb10d5ab64b53dbd0d1b3a062affc9
deveploper99 <rtxdeveploper99@gmail.com>
2025-10-16 13:03:50 +0600

    Intent to edit Profile page

--- app/src/main/java/com/example/uikit/MainActivity.kt
+++ app/src/main/java/com/example/uikit/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.uikit
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,11 +12,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_edit_profile)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(R.layout.activity_main)
+        val click = findViewById<ImageView>(R.id.imageView2)
+
+        click.setOnClickListener {
+            val intent = Intent(this, Edit_Profile::class.java)
+            startActivity(intent)
         }
     }
 }
